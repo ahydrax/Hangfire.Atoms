59cd5e91526f02392de194ff680d2351904c542b
Cédric Luthi <cedric.luthi@gmail.com>
2022-12-02 19:33:01 +0100

    Fix links to atom jobs in the atom state renderers
    Without using the UrlHelper, the link to _Atom details_ could be wrong and lead to HTTP 404 errors.
    
    For example, it would point to http://localhost:5000/jobs/atoms/10119 instead of http://localhost:5000/hangfire/jobs/atoms/10119 when using the default `app.UseHangfireDashboard()` method that maps the Hangfire dashboard on `/hangfire`.

--- src/Hangfire.Atoms/Dashboard/HtmlUtils.cs
+++ src/Hangfire.Atoms/Dashboard/HtmlUtils.cs
@@ -1,13 +1,22 @@
-﻿using Hangfire.Dashboard;
+﻿using System;
+using System.Reflection;
+using Hangfire.Dashboard;
 
 namespace Hangfire.Atoms.Dashboard
 {
     internal static class HtmlUtils
     {
+        private static readonly FieldInfo PageField = typeof(HtmlHelper).GetField("_page", BindingFlags.NonPublic | BindingFlags.Instance)
+                                                      ?? throw new MissingFieldException(typeof(HtmlHelper).FullName, "_page");
+
         public static NonEscapedString JobIdLink(this UrlHelper helper, string jobId)
             => new NonEscapedString($"<a href=\"{helper.JobDetails(jobId)}\">{jobId}</a>");
 
         public static NonEscapedString AtomLink(this HtmlHelper helper, string jobAtomId)
-            => new NonEscapedString($"<a href=\"/jobs/atoms/{jobAtomId}\">{jobAtomId}</a>");
+        {
+            var url = ((RazorPage)PageField.GetValue(helper)).Url;
+            var href = url.To($"/jobs/atoms/{jobAtomId}");
+            return new NonEscapedString($"<a href=\"{href}\">{jobAtomId}</a>");
+        }
     }
 }
