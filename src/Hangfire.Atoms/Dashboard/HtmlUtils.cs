af17f6f556ade2569e222eae5638560b8d804dbe
Cédric Luthi <cedric.luthi@gmail.com>
2022-12-04 15:25:59 +0100

    Do not use reflection to get the UrlHelper from the HtmlHelper

--- src/Hangfire.Atoms/Dashboard/HtmlUtils.cs
+++ src/Hangfire.Atoms/Dashboard/HtmlUtils.cs
@@ -1,22 +1,31 @@
-﻿using System;
-using System.Reflection;
-using Hangfire.Dashboard;
+﻿using Hangfire.Dashboard;
 
 namespace Hangfire.Atoms.Dashboard
 {
     internal static class HtmlUtils
     {
-        private static readonly FieldInfo PageField = typeof(HtmlHelper).GetField("_page", BindingFlags.NonPublic | BindingFlags.Instance)
-                                                      ?? throw new MissingFieldException(typeof(HtmlHelper).FullName, "_page");
-
         public static NonEscapedString JobIdLink(this UrlHelper helper, string jobId)
             => new NonEscapedString($"<a href=\"{helper.JobDetails(jobId)}\">{jobId}</a>");
 
         public static NonEscapedString AtomLink(this HtmlHelper helper, string jobAtomId)
         {
-            var url = ((RazorPage)PageField.GetValue(helper)).Url;
+            var url = GetUrlHelper(helper);
             var href = url.To($"/jobs/atoms/{jobAtomId}");
             return new NonEscapedString($"<a href=\"{href}\">{jobAtomId}</a>");
+        }
+
+        private static UrlHelper GetUrlHelper(this HtmlHelper htmlHelper)
+        {
+            var page = new EmptyPage();
+            _ = htmlHelper.RenderPartial(page);
+            return page.Url;
+        }
+
+        private class EmptyPage : RazorPage
+        {
+            public override void Execute()
+            {
+            }
         }
     }
 }
