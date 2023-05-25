f1024d3453b8e97f13e95d752fa81cb70a92fb8a
Cédric Luthi <cedric.luthi@gmail.com>
2023-05-25 11:18:09 +0200

    Simplify accessing the UrlHelper
    Possible since https://github.com/HangfireIO/Hangfire/commit/1ae167e3ffc87429a64cf95201c40508101cf25c

--- src/Hangfire.Atoms/Dashboard/HtmlUtils.cs
+++ src/Hangfire.Atoms/Dashboard/HtmlUtils.cs
@@ -9,23 +9,8 @@ namespace Hangfire.Atoms.Dashboard
 
         public static NonEscapedString AtomLink(this HtmlHelper helper, string jobAtomId)
         {
-            var url = GetUrlHelper(helper);
-            var href = url.To($"/jobs/atoms/{jobAtomId}");
+            var href = helper.Page.Url.To($"/jobs/atoms/{jobAtomId}");
             return new NonEscapedString($"<a href=\"{href}\">{jobAtomId}</a>");
-        }
-
-        private static UrlHelper GetUrlHelper(this HtmlHelper htmlHelper)
-        {
-            var page = new EmptyPage();
-            _ = htmlHelper.RenderPartial(page);
-            return page.Url;
-        }
-
-        private class EmptyPage : RazorPage
-        {
-            public override void Execute()
-            {
-            }
         }
     }
 }
